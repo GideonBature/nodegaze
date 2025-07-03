4607c05fcbb0b7c67d5622137898a5b10778aace
Chuks Agbakuru <chuksagbakuru@gmail.com>
2025-07-03 17:10:40 +0100

    Implement Node Authentication

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,24 @@
 //! and registers all API routes and middleware.
 //! It orchestrates the application's startup and defines its overall structure.
 
-fn main() {
-    println!("Welcome to NodeGaze!");
+mod utils;
+mod errors;
+mod services;
+mod api;
+
+use axum::{routing::get, Router, response::Json};
+use serde_json::{Value, json};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root_handler))
+        .nest("/api/node", api::node::routes::node_router().await);
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_handler() -> Json<Value> {
+    Json(json!({ "message": "Welcome to  NodeGaze" }))
 }
